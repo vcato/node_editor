b3340dba58ca244bdd51d38fe714bc0eb0497a19
vaughn <vaughncato@gmail.com>
2017-12-14 21:21:01 -0500

    Cleaned up in Diagram class

--- diagram.hpp
+++ diagram.hpp
@@ -3,46 +3,49 @@
 #include "node.hpp"
 
 
-struct Diagram {
-  std::vector<std::unique_ptr<Node>> _node_ptrs;
+class Diagram {
+  public:
+    Node *findNode(NodeIndex);
+    void evaluate();
+    void evaluate(std::ostream &);
+    void
+      updateNodeEvaluation(
+        NodeIndex,
+        std::vector<bool> &evaluated_flags,
+        std::ostream &
+      );
+    void
+      evaluateLine(
+        Node &node,
+        int line_index,
+        int output_index,
+        std::ostream &,
+        int input_index,
+        int input_node
+      );
+    NodeIndex addNode(const std::string &text);
 
-  Node *findNode(NodeIndex);
-  void evaluate();
-  void evaluate(std::ostream &);
-  void
-    updateNodeEvaluation(
-      NodeIndex,
-      std::vector<bool> &evaluated_flags,
-      std::ostream &
-    );
-  void
-    evaluateLine(
-      Node &node,
-      int line_index,
-      int output_index,
-      std::ostream &,
-      int input_index,
-      int input_node
-    );
-  NodeIndex addNode(const std::string &text);
+    void deleteNode(NodeIndex index);
 
-  void deleteNode(NodeIndex index);
+    Node &node(NodeIndex);
 
-  Node &node(NodeIndex);
+    int nExistingNodes() const;
 
-  int nExistingNodes() const;
+    void
+      connectNodes(
+        NodeIndex output_node_index,
+        int output_index,
+        NodeIndex input_node_index,
+        int input_index
+      );
 
-  void
-    connectNodes(
-      NodeIndex output_node_index,
-      int output_index,
-      NodeIndex input_node_index,
-      int input_index
-    );
+    void setNodeText(int node_index,const std::string &);
 
-  void setNodeText(int node_index,const std::string &);
+    void removeInvalidInputs();
 
-  void removeInvalidInputs();
+    std::vector<NodeIndex> existingNodeIndices() const;
 
-  std::vector<NodeIndex> existingNodeIndices() const;
+  private:
+    std::vector<std::unique_ptr<Node>> _node_ptrs;
+
 };
