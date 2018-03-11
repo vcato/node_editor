deb666963d8fa97bbd1ab372a9d7ce5401df7b80
Vaughn Cato <vcato1701@yahoo.com>
2018-03-11 18:07:20 -0400

    The scene viewer now shows the body positions.

--- scenetree.cpp
+++ scenetree.cpp
@@ -1,8 +1,11 @@
 #include "scenetree.hpp"
 
+#include <sstream>
+
 
 using Body = Scene::Body;
 using std::cerr;
+using std::ostringstream;
 
 
 void SceneTree::notifySceneChanged()
@@ -11,13 +14,38 @@ void SceneTree::notifySceneChanged()
 }
 
 
+static SceneTree::Item &addItemTo(SceneTree::Item &parent_item)
+{
+  parent_item.children.emplace_back();
+  return parent_item.children.back();
+}
+
+
 static void
-  addBodiesTo(SceneTree::Item &parent_item,const Scene::Bodies &bodies)
+  addBodiesTo(
+    SceneTree::Item &parent_item,
+    const Scene::Bodies &bodies,
+    const Scene::Frame &frame
+  )
 {
   for (auto &body : bodies) {
-    parent_item.children.emplace_back();
-    parent_item.children.back().label = body.name;
-    addBodiesTo(parent_item.children.back(),body.children);
+    auto &new_body_item = addItemTo(parent_item);
+    new_body_item.label = body.name;
+    auto &new_position_item = addItemTo(new_body_item);
+    new_position_item.label = "Position:";
+    {
+      auto &new_x_item = addItemTo(new_position_item);
+      ostringstream stream;
+      stream << "X: " << body.position.x(frame);
+      new_x_item.label = stream.str();
+    }
+    {
+      auto &new_y_item = addItemTo(new_position_item);
+      ostringstream stream;
+      stream << "Y: " << body.position.y(frame);
+      new_y_item.label = stream.str();
+    }
+    addBodiesTo(new_body_item,body.children,frame);
   }
 }
 
@@ -30,7 +58,7 @@ static SceneTree::Item makeItems(const Scene *scene_ptr)
     return root;
   }
 
-  addBodiesTo(root,scene_ptr->bodies());
+  addBodiesTo(root,scene_ptr->bodies(),scene_ptr->displayFrame());
 
   return root;
 }
