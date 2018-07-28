8af08898f1dd14f89335634e3e8b4ed02500426f
Vaughn Cato <vcato1701@yahoo.com>
2018-07-28 11:30:50 -0400

    Changed the default diagrams.
    Removed the Z value from the default local position diagram.

--- defaultdiagrams.cpp
+++ defaultdiagrams.cpp
@@ -203,7 +203,7 @@ diagram {
 Diagram localPositionDiagram()
 {
   Diagram diagram;
-  NodeIndex vector_index = diagram.addNode("[$,$,$]");
+  NodeIndex vector_index = diagram.addNode("[$,$]");
   diagram.node(vector_index).setPosition({100,180});
 
   NodeIndex x_index = diagram.addNode("x");
@@ -213,10 +213,6 @@ Diagram localPositionDiagram()
   NodeIndex y_index = diagram.addNode("y");
   diagram.node(y_index).setPosition({20,150});
   diagram.connectNodes(y_index,0,vector_index,1);
-
-  NodeIndex z_index = diagram.addNode("z");
-  diagram.node(z_index).setPosition({20,100});
-  diagram.connectNodes(z_index,0,vector_index,2);
 
   NodeIndex local_postion_index = diagram.addNode("local_position=$");
   diagram.node(local_postion_index).setPosition({230,150});
